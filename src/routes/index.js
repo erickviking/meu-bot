422e4bfdde9988bb2ec7d64127eb8e0136667661
erickviking <122755820+erickviking@users.noreply.github.com>
2025-07-13 20:18:45 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const express = require('express');
 const webhookRoutes = require('./webhook.routes');
 const conversationRoutes = require('./conversation.routes');
 const patientRoutes = require('./patients.routes'); // <-- 1. IMPORTAR A NOVA ROTA
+const clinicRoutes = require('./clinics.routes'); // 1. IMPORTAR
 
 const router = express.Router();
 
 router.use('/', webhookRoutes);
 router.use('/api/v1/conversations', conversationRoutes);
 router.use('/api/v1/patients', patientRoutes); // <-- 2. USAR A NOVA ROTA
+router.use('/api/v1/clinics', clinicRoutes); // 2. USAR
 
 module.exports = router;
